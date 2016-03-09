5e6f3eef3e77a95ab1c24e799a56eed7f29391fc
huangziyi01 <huangziyi01@baidu.com>
2016-03-09 20:31:14 +0800

    quick push

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Badge extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        <div>badge</div>
+    }
+}
+
+Badge.defaultProps = {}
