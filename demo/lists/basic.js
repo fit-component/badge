cf29fb2ec2695047c431d3d3f4c5e9c8a94cd562
huangziyi01 <huangziyi01@baidu.com>
2016-03-16 11:04:19 +0800

    quick push

---
+++ demo/lists/basic.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Badge from 'fit-badge'
+
+export default class Demo extends React.Component {
+    render() {
+        return (
+            <Badge count={5}>
+                213
+            </Badge>
+        )
+    }
+}
