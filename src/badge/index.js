aacc04635d8905b4578ee8bdabc62d473809a51c
huangziyi01 <huangziyi01@baidu.com>
2016-03-25 23:07:42 +0800

    update

--- src/badge/index.js
+++ src/badge/index.js
@@ -10,7 +10,7 @@ export default class Badge extends React.Component {
 
         let countAfterCalculation = count > overflowCount ? `${overflowCount}+` : count
 
-        // dot 不展示 count5677722
+        // dot 不展示 count5677722324
         if (dot) {
             countAfterCalculation = ''
         }
